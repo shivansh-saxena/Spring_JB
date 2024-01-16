c2dffb25caf98c85f531b0426b599f35274d1fe0
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-01-16 23:01:42 +0530

    adding primary methods to TopicController

--- src/main/java/com/shiv/controller/TopicController.java
+++ src/main/java/com/shiv/controller/TopicController.java
@@ -3,9 +3,7 @@ package com.shiv.controller;
 import com.shiv.model.Topic;
 import com.shiv.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,4 +21,19 @@ public class TopicController {
     {
         return topicService.getTopic(id);
     }
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+    }
+    @RequestMapping(method = RequestMethod.PUT,path = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic,@PathVariable int id)
+    {
+        topicService.updateTopic(id,topic);
+    }
+    @RequestMapping(method = RequestMethod.DELETE,path = "/topics/{id}")
+    public void deleteTopic(@PathVariable int id)
+    {
+        topicService.deleteTopic(id);
+    }
+
 }
--- src/main/java/com/shiv/service/TopicService.java
+++ src/main/java/com/shiv/service/TopicService.java
@@ -3,15 +3,16 @@ package com.shiv.service;
 import com.shiv.model.Topic;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic(1,"topic1"),
             new Topic(2,"topic2")
-    );
+    ));
     public List<Topic> getTopics(){
         return topics;
     }
@@ -19,5 +20,23 @@ public class TopicService {
     public Topic getTopic(int id) {
         return topics.stream().filter(t->
                 t.getId()==id).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(int id, Topic topic) {
+        for (Topic t:topics)
+        {
+            if(id==t.getId()) {
+                topics.set(id, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(int id) {
+        topics.removeIf(t-> t.getId()==id);
     }
 }
